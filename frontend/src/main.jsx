8814e6926ea2085d57d10e29cb47fe8277065ba8
sourabh jha <sjha0164@gmail.com>
2024-12-31 15:18:59 +0530

    more features added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import 'sweetalert2/src/sweetalert2.scss'
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import  Swal  from 'sweetalert2'
 
 const initialState = {
   cartItems: [],
@@ -10,17 +11,37 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const existingItem = state.cartItems.find(
-        (item) => item.id === action.payload._id
+        (item) => item._id === action.payload._id
       );
       if(!existingItem){
         state.cartItems.push(action.payload)
-        alert("item added successfully")
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Product added to the cart",
+          showConfirmButton: false,
+          timer: 1500
+        });
       }else{
-        alert("Item already exists")
+        Swal.fire({
+          title: "Already added to the cart",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Ok!"
+        });
       }
     },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(item => item._id !== action.payload._id)
+    },
+    clearCart: ( state ) => {
+      state.cartItems = []
+    }
   },
 });
 
-export const { addToCart } = cartSlice.actions
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions
 export default cartSlice.reducer
